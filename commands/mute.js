a92435e5f91080f373a66ae36a1bfa167edb36c3
DerpyDuck <Timmy49659@aol.com>
2018-03-21 09:50:35 -0700

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -18,8 +18,10 @@ module.exports.run = async (bot,message,args) => {
     if (mutee.user.bot) return message.channel.send("You cannot mute a bot");
     if (mutee.highestRole.position >= message.member.highestRole.position) return message.channel.send("You cannot mute someone with a higher/same role as you");
 
+    //Does the actual muting
     let role = message.guild.roles.find(r => r.name === "DDA Muted")
     if (!role) {
+        //Create new role wih no permissions and create channel overrides for that role
         try{
             let role = await message.guild.createRole({
                 name: "DDA Muted",
@@ -29,7 +31,8 @@ module.exports.run = async (bot,message,args) => {
             message.guild.channels.forEach(async (channel,id) => {
                 await channel.overwritePermissions(role, {
                     SEND_MESSAGES: false,
-                    ADD_REACTIONS: false
+                    ADD_REACTIONS: false,
+                    SPEAK: false
                 });
             });
         } catch(e) {
@@ -38,6 +41,7 @@ module.exports.run = async (bot,message,args) => {
     }
     if (mutee.roles.has(role.id)) return message.channel.send(`${mutee.user.username} is already muted`)
 
+    //Allows to specify time
     await mutee.addRole(role);
     if (args[1]) {
         let timeAmount = args[1].substring(args[1].length-1,args[1].length);
@@ -47,6 +51,7 @@ module.exports.run = async (bot,message,args) => {
             guild: message.guild.id,
             time: Date.now() + parseInt(args[1]) * timeTable[timeAmount][0]
         }
+        //Writes to json file with when mutee can be unmuted
         fs.writeFile("./mutes.json", JSON.stringify(bot.mutes, null, 4), err =>{
             if (err) throw err;
             message.channel.send(`${mutee.user.username} has been muted for ${time} ${timeTable[timeAmount][1]}`)
