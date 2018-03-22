61ec7bdee0ec134afc0d6d820530d2770cfbea26
unknown <Timmy49659@aol.com>
2018-03-21 23:06:41 -0700

    a

--- commands/music.js
+++ commands/music.js
@@ -7,6 +7,7 @@ const youtube = new YouTube(process.env.googleapikey)
 
 module.exports.run = async (bot,message,args) => {
     const url = args[1].replace(/<.+>/g, "$1");
+    console.log(url)
     const serverQueue = queue.get(message.guild.id);
     //Play
     if (args[0] === "play") {
