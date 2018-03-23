5932db75477f450419f97bb4a8ae1a9a6eef90ad
unknown <Timmy49659@aol.com>
2018-03-22 23:55:57 -0700

    a

--- commands/help.js
+++ commands/help.js
@@ -1,36 +1,38 @@
 const Discord = module.require("discord.js");
-const cmdlist = [
-    "avatar",
-    "userinfo",
-    "ping",
-    "say",
-    "music",
-    "weather",
-    "coinflip",
-    "dieroll",
-    "diceroll",
-    "randnum",
-    "purge",
-    "striproles",
-    "mute",
-    "unmute",
-    "kick"
-]
+const object = new Object();
+const helplist = {
+    "Generic":["avatar","userinfo","ping","say","weather"],
+    "Random":["coinflip","dieroll","diceroll","randnum"],
+    "Moderation":["purge","striproles","mute","unmute","kick"],
+    "Music":["music"]
+}
 
 module.exports.run = async (bot,message,args) => {
-    let cmds = "";
-    cmdlist.forEach((v,i) => {
-        if (i < cmdlist.length-1) {
-            cmds += `\`${cmdlist[i]}\`, `;
-        } else {
-            cmds += `\`${cmdlist[i]}\``;
-        }
-    });
+    let cmds = [];
+    for (let [k,v] of object.entries(helplist)) {
+        let subcom = "";
+        v.forEach((v,i) => {
+            if (i < v.length-1) {
+                subcom += `\`${v[i]}\`, `;
+            } else {
+                subcom += `\`${v[i]}\``;
+            }
+        });
+        cmds.push(cmds, {
+            name:`**${k}**`,
+            value:subcom
+        });
+    }
     let embed = new Discord.RichEmbed()
         .setAuthor("Available Commands")
         .setDescription(cmds)
-        .setColor("#9B59B6")
-    message.channel.send({embed: embed})
+        .setColor("#9B59B6");
+    message.channel.send({embed: {
+        title:"Available Commands",
+        description:"Now THIS is de wey!",
+        color:"#9B59B6",
+        fields:cmds
+    }});
 }
 
 module.exports.help = {
