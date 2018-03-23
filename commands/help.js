01ba584e3457299951ba09d60425828724ac4637
unknown <Timmy49659@aol.com>
2018-03-23 00:19:08 -0700

    a

--- commands/help.js
+++ commands/help.js
@@ -8,26 +8,43 @@ const helplist = {
 }
 
 module.exports.run = async (bot,message,args) => {
-    let cmds = [];
-    for (let k in helplist) {
-        let subcom = "";
-        helplist[k].forEach((v,i) => {
-            if (i < v.length-1) {
-                subcom += `\`${helplist[k][i]}\`, `;
-            } else {
-                subcom += `\`${helplist[k][i]}\``;
-            }
-        });
-        cmds.push(cmds, {
-            name:`**${k}**`,
-            value:subcom
-        });
-    }
+    // let cmds = [];
+    // for (let k in helplist) {
+    //     let subcom = "";
+    //     helplist[k].forEach((v,i) => {
+    //         if (i < v.length-1) {
+    //             subcom += `\`${helplist[k][i]}\`, `;
+    //         } else {
+    //             subcom += `\`${helplist[k][i]}\``;
+    //         }
+    //     });
+    //     cmds.push(cmds, {
+    //         name:`**${k}**`,
+    //         value:subcom
+    //     });
+    // }
     message.channel.send({embed: {
         title:"Available Commands",
         description:"Now THIS is de wey!",
         color:"#9B59B6",
-        fields:cmds
+        fields: [
+            {
+                name:"**Generic**",
+                value:"`avatar`, `userinfo`, `ping`, `say`, `weather`"
+            },
+            {
+                name:"**Random**",
+                value:"`coinflip`, `dieroll`, `diceroll`, `randnum`" 
+            },
+            {
+                name:"**Moderation**",
+                value:"`purge`, `striproles`, `mute`, `unmute`, `kick`"
+            },
+            {
+                name:"**Music**",
+                value:"`music`"
+            }
+        ]
     }});
 }
 
