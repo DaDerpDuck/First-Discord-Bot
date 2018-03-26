0cdd0d7d08fda73b8327c5dfd145bcd2de6c3d58
DerpyDuck <Timmy49659@aol.com>
2018-03-26 10:32:08 -0700

    Update pretend.js

--- commands/pretend.js
+++ commands/pretend.js
@@ -1,6 +1,8 @@
 module.exports.run = async (bot,message,args) => {
+    const target = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]) || message.guild.member(message.author);
+    if (!target.bot) return message.channel.send("Sorry, this command only works on bots!");
     message.delete();
-    message.channel.send(args.join(" "));
+    message.channel.send(args.shift().join(" "));
 }
 
 module.exports.help = {
