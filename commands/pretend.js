146c81710d5f3f7da5f1a3322d24392168f9d9ee
DerpyDuck <Timmy49659@aol.com>
2018-03-26 11:03:21 -0700

    Update pretend.js

--- commands/pretend.js
+++ commands/pretend.js
@@ -2,7 +2,42 @@ module.exports.run = async (bot,message,args) => {
     const target = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]) || message.guild.member(message.author);
     if (!target.bot) return message.channel.send("Sorry, this command only works on bots!");
     message.delete();
-    message.channel.send(args.shift().join(" "));
+    hook(message.channel,target.user.username,args.shift().join(" "), target.user.displayAvatarURL);
+    return;
+}
+
+function hook(channel,title,message,avatar) {
+    if (!message) return;
+    channel.fetchWebhooks()
+        .then(webhook => {
+            let foundHook = webhook.find("name","DaDerpDuck's Webhook")
+            
+            if (!foundHook) {
+                channel.createWebhook("DaDerpDuck's Webhook","https://ih1.redbubble.net/image.222712334.4772/flat,800x800,075,f.jpg")
+                    .then(webhook => {
+                        webhook.send(message, {
+                            "username": title,
+                            "avatarurl": avatar,
+                        });
+                    })
+                        .catch(error => {
+                            console.log(error);
+                            return channel.send("Something went wrong.. :(");
+                    });
+            } else {
+                channel.createWebhook("DaDerpDuck's Webhook","https://ih1.redbubble.net/image.222712334.4772/flat,800x800,075,f.jpg")
+                    .then(webhook => {
+                        webhook.send(message, {
+                            "username": title,
+                            "avatarurl": avatar,
+                        });
+                    })
+                        .catch(error => {
+                            console.log(error);
+                            return channel.send("Something went wrong.. :(");
+                    });
+            }
+        });
 }
 
 module.exports.help = {
